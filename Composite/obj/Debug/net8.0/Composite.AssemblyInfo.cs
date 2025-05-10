98ad2d9241aa2ef2f156690b046757e0a4d1481a
vt231_kso <vt231_kso@student.ztu.edu.ua>
2025-05-10 20:41:54 +0300

    Реалізовано хуки життєвого циклу для елементів за допомогою Шаблонного Методу

--- Composite/obj/Debug/net8.0/Composite.AssemblyInfo.cs
+++ Composite/obj/Debug/net8.0/Composite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Composite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca7038c7e07b0344189b33dc4816d7e4f1fb08f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba6e672afaccf03c08f66ab7e036aab8303b102f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Composite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Composite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
